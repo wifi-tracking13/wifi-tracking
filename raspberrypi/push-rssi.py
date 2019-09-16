757f58a418f560c8fa8537ee17df92abbf043316
nghia <nghia.le4@tamu.edu>
2019-09-16 18:14:32 -0500

    Pushes hashed MAC to database

--- raspberrypi/push-rssi.py
+++ raspberrypi/push-rssi.py
@@ -10,7 +10,7 @@ connection = mysql.connector.connect(host='192.168.137.1',
                                      user='brandon',
                                      password='password')
 try:
-    for x in range(100):
+    while True:
         #remove first 3 rows of cvs file, unneccesary 
         df=pd.read_csv("../data/capture-01.csv", skiprows=3)
         #deletes white space and colons from MAC address
@@ -23,27 +23,21 @@ try:
         for index, row in new_df.iterrows():
             foundMacAddress = False;
             currentMac = row['Station MAC']
-            print(row['Station MAC'], row['Last time seen'], row['Power'])
+            print('Searching for MAC address {} in database...'.format(currentMac))
             cursor = connection.cursor()
-            cursor.execute("""SELECT mac_address, enabled, device_name FROM registered_macs WHERE enabled = 1""")
+            cursor.execute("""SELECT mac_address FROM registered_macs WHERE enabled = 1""")
             cursorRow = cursor.fetchone()
             while cursorRow:
-                print('device name: {} and is enabled: {}'.format(cursorRow[2], cursorRow[1]))
                 currentStoredMac = cursorRow[0]
                 if bcrypt.checkpw(currentMac.encode('utf8'), currentStoredMac.encode('utf8')):
-                    print('Found MAC address '+row['Station MAC']+ ' in database, storing rssi value...')
+                    print('Found! Storing RSSI value now...'.format(currentMac))
                     foundMacAddress = True
                 cursorRow = cursor.fetchone()
             if (foundMacAddress):
-                print('found')
+                cursor.execute("""INSERT INTO rssi (pi_id, mac_address, last_seen, power) VALUES (%s, %s, %s, %s)""", ("1", bcrypt.hashpw(currentMac.encode('utf8'), bcrypt.gensalt()), row['Last time seen'], row['Power']))
                 connection.commit()
-                    #cursor.execute("""INSERT INTO
-                    #connection.commit()
             else:
-                print('MAC address ' +row['Station MAC']+ ' not found')
                 connection.commit()
-            #new_df.to_csv("../data/db-cap.csv", index = False)
-            time.sleep(1)
 finally:
     if(connection.is_connected()):
         cursor.close()
